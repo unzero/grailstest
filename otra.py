cc136f253eaf087441442ad36b6b0512d00fec6e
Ciro Garcia <ciroivan94@gmail.com>
2016-03-12 12:50:04 -0500

    nuevo file en python

---
+++ otra.py
@@ -0,0 +1,17 @@
+
+import sys
+
+
+def main():
+    
+
+def read_int():
+    return int(raw_input())
+
+def read_vector():
+    return raw_input().split()
+
+def read_all():
+    return sys.stdin.read().splitlines()
+
+main()
